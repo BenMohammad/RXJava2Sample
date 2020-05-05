c2bd611d47577939af4404f8039661769d0e0c21
Ben Mohammad <benmohammad593@gmail.com>
2020-05-05 23:02:26 +0100

    PaginationAdapter added...

---
+++ app/src/main/java/com/rxjava2sample/ui/pagination/PaginationAdapter.java
@@ -0,0 +1,56 @@
+package com.rxjava2sample.ui.pagination;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.rxjava2sample.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PaginationAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    List<String> items = new ArrayList<>();
+    public PaginationAdapter(){}
+
+    void addItems(List<String> items) {
+        this.items = items;
+
+    }
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return ItemViewHolder.create(parent);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        ((ItemViewHolder)holder).bind(items.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    private static class ItemViewHolder extends RecyclerView.ViewHolder {
+        ItemViewHolder(View itemView) {
+            super(itemView);
+        }
+
+        static ItemViewHolder create(ViewGroup parent) {
+            return new ItemViewHolder(
+                    LayoutInflater.from(parent.getContext()).inflate(R.layout.item_pagination, parent, false));
+
+        }
+
+        void bind(String content) {
+            ((TextView) itemView).setText(content);
+        }
+    }
+}
