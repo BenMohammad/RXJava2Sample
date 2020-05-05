db8b9f841e4aba98b09c62af7852413591906e0b
Ben Mohammad <benmohammad593@gmail.com>
2020-05-05 22:31:56 +0100

    SelectionActivity on click RxBus added....

--- app/src/main/java/com/rxjava2sample/ui/SelectionActivity.java
+++ app/src/main/java/com/rxjava2sample/ui/SelectionActivity.java
@@ -2,9 +2,13 @@ package com.rxjava2sample.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.rxjava2sample.MyApplication;
 import com.rxjava2sample.R;
+import com.rxjava2sample.ui.rxbus.RxBusActivity;
 
 public class SelectionActivity extends AppCompatActivity {
 
@@ -12,5 +16,10 @@ public class SelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
+    }
+
+    public void startRxBusActivity(View view) {
+        ((MyApplication) getApplication()).sendAutoEvent();
+        startActivity(new Intent(SelectionActivity.this, RxBusActivity.class));
     }
 }
